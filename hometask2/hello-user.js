7a61e1e15a7803c0f2179d53ff904885cabcc5f9
Anastasiia Kholod <asyaruda06@gmail.com>
2023-05-24 23:29:47 +0300

    Ready for review

---
+++ hometask2/hello-user.js
@@ -0,0 +1,13 @@
+const input = prompt ('What is your name?')
+console.log(input)
+
+const result = alert('Hello John, how are you!')
+console.log(result)
+
+const yesOrNo = confirm('Your name is really John?')
+if (yesOrNo) {
+    alert('Yes, for sure')
+}
+else{
+    alert('Hello noname. How are you?')
+}
